a28649d17a8626a8ad43d1b3b56b7fc2bf734d2f
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-12-28 18:46:13 +0500

    Learn basic types and arrays

---
+++ basic/app.js
@@ -0,0 +1,16 @@
+//basic types
+var str = "string type";
+var num = 1;
+var t = true;
+var anytype;
+var str_or_num;
+//arrays
+var NumArray = [1, 2, 3];
+var NumArray2 = [1, 2, 3];
+var BoolArray = [true, false];
+var BoolArray2 = [true, false];
+var BoolArray3;
+BoolArray3 = [true, false];
+//tuples
+var tuple1 = [1, "1"];
+var tuple2 = [true, 1, "1"];
---
+++ basic/app.ts
@@ -0,0 +1,18 @@
+//basic types
+let str: string = "string type";
+let num: number = 1;
+let t: boolean = true;
+let anytype: any;
+let str_or_num: string | number;
+
+//arrays
+let NumArray: number[] = [1, 2, 3];
+let NumArray2: Array<number> = [1, 2, 3];
+let BoolArray = [true, false];
+let BoolArray2: boolean[] = [true, false];
+let BoolArray3: Array<boolean>
+BoolArray3 = [true, false]
+
+//tuples
+let tuple1: [number, string] = [1, "1"];
+let tuple2: [boolean, number, string] = [true, 1, "1"];
